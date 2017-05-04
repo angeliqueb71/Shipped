0ff6d4b6e76d06f1f0f0c86d8d652ee25cccf45d
Angelique Barth <angeliqueb71@aol.com>
2017-05-04 17:47:39 -0400

    added Registrations Controller/migration and and fname, lname to devise user

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   resources :job
   resources :ship_job
 
-  devise_for :users
+
+  devise_for :users, :controllers => { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
 
